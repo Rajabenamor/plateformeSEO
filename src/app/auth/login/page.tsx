2f4751ab48cb616fe6ca52708669812a65741b1e
Raja <rajabenamor538@gmail.com>
2026-03-10 00:32:56 +0100

    password reset link with email customisation

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { loginServerAction } from "@/app/actions/auth";
 import { LoginFormData, loginSchema } from "@/app/types/auth";
+import Link from "next/link";
 
 
 export default function LoginPage() {
@@ -31,43 +32,50 @@ export default function LoginPage() {
       setError(result.error);
     }
   };
-    return (
-      <div>
-        
-        <h2>Log In to Your Dashboard</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* 4. Conditionally display the error message to the user */}
-          {error && <div className="color-red mb-10">{error}</div>}
-          <input
-            {...register("username")}
-            type="text"
-            name="username"
-            placeholder="Username"
-      
-          />
-          {errors.username && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.username.message}
-            </p>
-          )}
-          {/* <input type="email" name="email" placeholder="Email" required /> */}
-          <input
-            {...register("password")}
-            type="password"
-            name="password"
-            placeholder="Password"
-        
-          />
-          {errors.password && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.password.message}
-            </p>
-          )}
-          <button type="submit">
-            {isSubmitting ? "Signing in..." : "Login"}
-          </button>
-        </form>
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white border border-gray-200 rounded-xl shadow-sm">
+     <div className="py-8">
+      <h2 className="text-2xl font-bold text-gray-900">Log In to Your Dashboard</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* 4. Conditionally display the error message to the user */}
+        {error && <div className="color-red mb-10">{error}</div>}
+        <input
+          {...register("username")}
+          type="text"
+          name="username"
+          placeholder="Username"
+          className="w-full px-4 py-2.5 mt-3 text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors"
+        />
+        {errors.username && (
+          <p className="text-red-500 text-xs mt-1">{errors.username.message}</p>
+        )}
+        {/* <input type="email" name="email" placeholder="Email" required /> */}
+        <input
+          {...register("password")}
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="w-full px-4 py-2.5 mt-4 text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors"
+        />
+        {errors.password && (
+          <p className="text-red-500 text-xs mt-1">{errors.password.message}</p>
+        )}
+        {/*Forgot password link */}
+        <div className="flex justify-end mt-2">
+          <Link
+            href="/auth/forgot-password"
+            className="text-xs font-medium text-blue-600 hover:text-blue-500 transition-colors duration-200"
+          >
+            Forgot password?
+          </Link>
+        </div>
+        <button type="submit"
+        className="w-full mt-2 px-4 py-2.5 text-white font-medium bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 transition-colors"
+        >
+          {isSubmitting ? "Signing in..." : "Login"}
+        </button>
+      </form>
       </div>
-    );
-  };
-
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function navbar() {
   return (
-    <nav className="border-b border-slate-100 bg-yellow-900">
+    <nav className="border-b border-slate-100 bg-blue-300">
       <div className="max-w-6xl mx-auto px-6 h-20 flex items-center justify-between">
         <Link href="/auth/login">Sign In</Link>
         <Link href="/auth/register">Sign Up</Link>
